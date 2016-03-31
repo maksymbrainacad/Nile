572b482f701d58d1d4f0930cfb1f96e0c294b0c6
PioneerP <forbusiness2012@yandex.ua>
2016-03-31 16:03:54 +0300

    text area finish

--- evg-g/hw/dom/textArea.js
+++ evg-g/hw/dom/textArea.js
@@ -2,22 +2,33 @@
 window.addEventListener('load', function() {
   var textAreaNode = document.querySelector('[data-id="textArea"]'),
       actionsNode = document.querySelector('[data-id="actions"]'),
-      animationText = document.querySelector('[data-id="animationText"]');
+      animationTextNode = document.querySelector('[data-id="animationText"]');
 
   actionsNode.addEventListener('click', function(e) {
 
-      var textAreaInput = textAreaNode.value;
-      var transformText = textAreaInput.split( );
-      var transformTextLength = transformText.length;
-      var i = 0;
+      var textAreaInput = textAreaNode.value,
+          transformText = textAreaInput.split(' '),
+          transformTextLength = transformText.length,
+          transformTextArr =[],
+          i = 0;
 
       for (; i < transformTextLength; i++) {
-          
+        transformTextArr[i] = '<span>' + transformText[i] + '</span>';
       }
 
-      document.querySelector('[data-id="animationText"]').innerHTML = textAreaInput;
+      transformTextArr = transformTextArr.join(' ');
 
-      console.log(textAreaInput);
+      document.querySelector('[data-id="animationText"]').innerHTML = transformTextArr;
+
+    e.stopPropagation();
+    return false;
+  });
+
+  animationTextNode.addEventListener('click', function(e) {
+    if (e.target !== e.currentTarget) {                           // определили на каком месте по странице был клик
+       e.target.style.display = 'none';
+    }
+
     e.stopPropagation();
     return false;
   });
