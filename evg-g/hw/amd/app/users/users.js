1bf6fa828ed448c8e29faeadbe6bfd02278df3ed
PioneerP <forbusiness2012@yandex.ua>
2016-04-26 21:43:29 +0300

    amd2

--- evg-g/hw/amd/app/users/users.js
+++ evg-g/hw/amd/app/users/users.js
@@ -1,5 +1,7 @@
 define(['data/user.js'], function(user) {
   return function() {
     console.log('User page');
+    
+    return user;
   }
 });
--- evg-g/hw/amd/data/user.js
+++ evg-g/hw/amd/data/user.js
@@ -1,4 +1,19 @@
 define({
   id: 1,
-  name: 'Piter'
+  name: 'Piter',
+  relative: [
+    {
+      id: 2,
+      name: 'Ben'
+    },
+    {
+      id: 3,
+      name: 'Armen'
+    },
+    {
+      id: 4,
+      name: 'Kolya',
+      active: true
+    }
+  ]
 });
