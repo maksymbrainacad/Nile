605dfae09407f7d6ca6f6ecf31163319b6a39388
PioneerP <forbusiness2012@yandex.ua>
2016-04-28 21:42:17 +0300

    bootstrap fw

---
+++ evg-g/hw/servReqGit/js/$ajax.js
@@ -0,0 +1,56 @@
+function $ajax(obj) {                           // функция формирующая запрос на сервер
+  /*                                      // описание принимаемых данных
+    obj: {
+      method: 'GET'|'POST'|'POST'|'PUT'|'DELETE'
+        | default: 'GET'
+      url: String
+      async: true|false
+      | default: true
+      responseType:
+      | default: 'text'
+      success: function() {}
+      error: function() { console.error (responseText)}
+      data: {}
+      | default: null
+  }
+  */
+  if (!obj) {            // проверка, если пользователь не ввел данные при запросе
+    obj = {};
+  }
+
+  var method = obj.method || 'GET',
+      url = obj.url,
+      asynchronus = obj.async || true,
+      responseType = obj.responseType || 'text',
+      success = obj.success,
+      error = obj.error || function (req, aEvt) { console.error(req, aEvt); },
+      data = obj.data || null;
+
+      var req = new (XMLHttpRequest || ActiveXObject)();                   // в переменную положили команду создающую новый запрос на сервер
+                                                                          //  c проверкой и поддержкой браузера ИЕ
+
+      req.open(method, url, asynchronus);      // open - открыть новое соединение и сформируем запрос на сервер
+
+      // req.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); // отправляем заголовок X-Requested-With запроса с содержанием XMLHttpRequest для сервера
+
+      req.onreadystatechange = function (aEvt) {      // обработка запроса на ошибки после выполнения
+        if (req.readyState === 4) {                   // readyState статус готовности к приемке запроса
+          if (req.status === 200) {                   // status статус ответа от сервера
+            if (responseType === 'json') {            // проверяем передаваемые данные на json содержимое
+              var responseJson;
+
+              try {
+                responseJson = JSON.parse(req.responseText);
+              } catch (e) {
+
+              }
+            }
+            success && success(responseJson || req.responseText, req);
+          } else {
+            error(req, aEvt);
+          }
+        }
+      };
+
+      req.send(data);
+}
