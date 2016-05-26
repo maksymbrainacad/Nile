a7f086396deb6f6b62224566891c55c9f340b07e
PioneerP <forbusiness2012@yandex.ua>
2016-05-26 21:49:48 +0300

    grunt 2

---
+++ evg-g/hw/tableSort/.temp/app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('usersList', ['ngRoute', 'components'])   // создали angular приложение testApp, второй параметр принимает массив зависимостей
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {               // когда пользователь находиться на первой странице, подгрузить для нее шаблон вью и навесить контроллеры
+        controller: 'MainController',
+        templateUrl: '/app/views/main.html'
+      })
+      .when('/users/:id', {        // :id - создает переменную id и ложит в нее все что ввел пользователь после /users/
+        controller: 'UserController',
+        templateUrl: '/app/views/user.html'
+      })
+      .otherwise({             // в противном случае
+        redirectTo: '/'         // redirectTo - перенаправить пользователя на страницу '/'
+      });
+});
